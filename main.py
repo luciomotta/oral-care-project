fdb1e3f7c9db11ef7a95796e3ba634cd090c9cf4
luciomotta <83682095+luciomotta@users.noreply.github.com>
2023-10-17 10:20:18 -0300

    Update main.py

--- main.py
+++ main.py
@@ -6,9 +6,12 @@ from Modules.dados import pegar_dados
 
 # Para exeutar:
 # streamlit run main.py
-texto = "testando"
+texto = "testando SSL"
+
 df = pegar_dados()
 
+st.write("# Análise de Dados Oral Care")
+st.write("Aqui você pode visualizar e analisar os dados da pesquisa Oral Care.")
 texto
 df
 
